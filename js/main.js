a1994acadd9170e8016ee910d86e1d117a886459
Johan Sundin <Johan Sundin>
2016-10-02 22:01:49 +0200

    light up carrot and multiple galleries

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ window.onload = function () {
 
   var Viewer = window.Viewer;
   var console = window.console || { log: function () {} };
-  //var pictures = document.querySelectorAll('.docs-pictures > li');
-  var pictures = document.querySelector('.docs-pictures');
-  //var pictures = document.querySelectorAll('.docs-pictures');
+  //var pictures = document.querySelector('.docs-pictures');
+  
+  //var pictures = document.querySelector('.docs-galley');
+  var gallery1 = document.getElementById('gallery1');
+  var gallery2 = document.getElementById('gallery2');
   var toggles = document.querySelector('.docs-toggles');
   var buttons = document.querySelector('.docs-buttons');
   var options = {
@@ -69,75 +71,9 @@ window.onload = function () {
     }
   }
 
-  addEventListener(pictures, 'build', function (e) {
-    console.log(e.type);
-  });
-  addEventListener(pictures, 'built', function (e) {
-    console.log(e.type);
-  });
-  addEventListener(pictures, 'show', function (e) {
-    console.log(e.type);
-  });
-  addEventListener(pictures, 'shown', function (e) {
-    console.log(e.type);
-  });
-  addEventListener(pictures, 'hide', function (e) {
-    console.log(e.type);
-  });
-  addEventListener(pictures, 'hidden', function (e) {
-    console.log(e.type);
-  });
-  addEventListener(pictures, 'view', function (e) {
-    console.log(e.type, e.detail.index);
-  });
-  addEventListener(pictures, 'viewed', function (e) {
-    console.log(e.type, e.detail.index);
-  });
-  viewer = new Viewer(pictures, options);
+  //viewer = new Viewer(pictures, options);
+  new Viewer(gallery1, options);
+  new Viewer(gallery2, options);
+  //new Viewer(document.getElementById('gallery2'));
 
-  //toggleButtons(options.inline ? 'inline' : 'modal');
-/*
-  toggles.onchange = function (event) {
-    var e = event || window.event;
-    var input = e.target || e.srcElement;
-    var name;
-
-    if (viewer) {
-      name = input.getAttribute('name');
-      options[name] = name === 'inline' ? JSON.parse(input.getAttribute('data-value')) : input.checked;
-      viewer.destroy();
-      viewer = new Viewer(pictures, options);
-      //toggleButtons(options.inline ? 'inline' : 'modal');
-    }
-  };
-
-
-  buttons.onclick = function (event) {
-    var e = event || window.event;
-    var button = e.target || e.srcElement;
-    var method = button.getAttribute('data-method');
-    var target = button.getAttribute('data-target');
-    var args = JSON.parse(button.getAttribute('data-arguments')) || [];
-
-    if (viewer && method) {
-      if (target) {
-        viewer[method](target.value);
-      } else {
-        viewer[method](args[0], args[1]);
-      }
-
-      switch (method) {
-        case 'scaleX':
-        case 'scaleY':
-          args[0] = -args[0];
-          break;
-
-        case 'destroy':
-          viewer = null;
-          toggleButtons('none');
-          break;
-      }
-    }
-  };
-*/
 };
